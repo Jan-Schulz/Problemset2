9951530aaf83738c8ac08f3a745646103e6dd7f0
Jan-Schulz <janschulzheide@web.de>
2022-04-25 15:37:10 +0200

    so passt das wieder

--- Feature1.py
+++ Feature1.py
@@ -9,8 +9,6 @@ print("rofl lol")
 
 
 for x in list(1,2,3):
-<<<<<<< HEAD
-<<<<<<< HEAD
     print(1+x)
 
 sonne = True
@@ -19,11 +17,10 @@ if sonne == True:
     print("geil")
 else:
     print("kacke")
-=======
+
     x = x + 1
     print(x)
->>>>>>> Schleife erweitert
-=======
+
     x = x + 1
     print(x)
->>>>>>> 31b5a7b9469c21fe5dc4d8d0f629eeafdc80e3bb
+
